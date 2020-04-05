17621ecdda50e071c0d60986d6a62105c4bfc553
Ganeshk750 <ganesh9ot@gmail.com>
2020-04-05 16:39:32 +0530

    comp life cycle method

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from 'react';
 
 /* This is functional Comp with useState Hooks */
-const App = () =>{
+/* const App = () =>{
  const [count, setCount] = useState(0);
 
  const increment = () =>{
@@ -13,7 +13,7 @@ const App = () =>{
      <button onClick={increment}>Clicked{count}times</button>
    </div>
  );
-};
+}; */
 
 /* This is class Comp */
 /* class App extends Component {
@@ -37,5 +37,35 @@ const App = () =>{
   }
    
 } */
+
+/* Here we discuss life cycle method with class comp */
+ class App extends Component {
+   state = {
+    count: 0
+   }
+   increment = () =>{
+     this.setState ({
+      count: this.state.count + 1
+     });
+   }
+   componentDidMount(){
+     document.title = `Clicked ${this.state.count} times`
+   }
+
+  componentDidUpdate(){
+    document.title = `Clicked ${this.state.count} times`
+   } 
+  render(){
+    return (
+      <div>
+      <h2>Counter App</h2>
+       <button onClick={this.increment}>Clicked{this.state.count}times</button>
+      </div>
+    )
+    
+  }
+   
+} 
+
 
 export default App;
